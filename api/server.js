ab7c71a2a3e576013e5043477c53d73e273f7858
Darrena Gray <40064931+WhatTheSpork@users.noreply.github.com>
2019-05-03 13:42:16 -0400

    Imported actions router and created actions base url

--- api/server.js
+++ api/server.js
@@ -5,12 +5,14 @@ const morgan = require('morgan');
 const server = express();
 
 const projectsRouter = require('../projects/projects-router');
+const actionsRouter = require('../actions/actions-router');
 
 server.use(express.json());
 server.use(helmet());
 server.use(morgan('dev'));
 
-server.use('/api/projects', projectsRouter)
+server.use('/api/projects', projectsRouter);
+server.use('/api/actions', actionsRouter);
 
 server.get('/', (req, res) => {
     res.send(`Connected!`);
