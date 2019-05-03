9aaf7f34bcae67099fc136e755b859a700a08ef9
Darrena Gray <40064931+WhatTheSpork@users.noreply.github.com>
2019-05-03 13:48:01 -0400

    Created endpoint POST for adding a new action to the database

--- actions/actions-router.js
+++ actions/actions-router.js
@@ -13,4 +13,16 @@ router.get('/', (req, res) => {
         });
 });
 
+router.post('/', (req, res) => {
+    const { project_id, description, notes } = req.body;
+    Actions
+        .addAction({ project_id, description, notes })
+        .then(action => {
+            res.status(201).json({ action, message: "The actions was succesfully added!" });
+        })
+        .catch(err => {
+            res.status(500).json({ err, message: "There was a problem adding the action." });
+        });
+});
+
 module.exports = router;
