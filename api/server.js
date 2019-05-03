88747ad904ac35aed64cf62c0a9b7ae8df7c8d0b
Darrena Gray <40064931+WhatTheSpork@users.noreply.github.com>
2019-05-03 11:53:29 -0400

    Index and server connected and running

---
+++ api/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const helmet = require('helmet');
+const morgan = require('morgan');
+
+const server = express();
+
+
+server.use(express.json());
+server.use(helmet());
+server.use(morgan('dev'));
+
+server.get('/', (req, res) => {
+    res.send(`Connected!`);
+});
+
+module.exports = server;
