e2e2c35636cc62184803fcb6e2faaf013dc46456
Darrena Gray <40064931+WhatTheSpork@users.noreply.github.com>
2019-05-03 14:58:43 -0400

    Created endpoints GET for retrieving project by id with its respective actions and retrieving only the actions of the specified project id

--- projects/projects-router.js
+++ projects/projects-router.js
@@ -13,6 +13,22 @@ router.get('/', (req, res) => {
         })
 })
 
+router.get('/:id', (req, res) => {
+    const { id } = req.params;
+    Projects
+        .getProject(id)
+        .then(project => {
+            if (project) {
+                res.status(200).json(project);
+            } else {
+                res.status(404).json({ message: "The project with the specified ID could not be found." });
+            }
+        })
+        .catch(err => {
+            res.status(500).json({ err, message: "The project could not be retrieved." });
+        });
+});
+
 router.post('/', (req, res) => {
     const projectInfo = req.body;
     Projects
@@ -22,6 +38,22 @@ router.post('/', (req, res) => {
         })
         .catch(err => {
             res.status(500).json({ err, message: "There was a problem adding this project." });
+        });
+});
+
+router.get('/:id/actions', (req, res) => {
+    const { id } = req.params;
+    Projects
+        .getProjectActions(id)
+        .then(project => {
+            if (project.length !== 0) {
+                res.status(200).json(project);
+            } else {
+                res.status(404).json({ message: "The project with the specified ID could not be found." });
+            }
+        })
+        .catch(err => {
+            res.status(500).json({ err, message: "There was a problem getting the actions for this project." });
         });
 });
 
