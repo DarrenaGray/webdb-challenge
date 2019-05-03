79d45ea0a95a1fcf31a89427fa3595bbde11f71f
Darrena Gray <40064931+WhatTheSpork@users.noreply.github.com>
2019-05-03 11:59:41 -0400

    Knex file configured

---
+++ knexfile.js
@@ -0,0 +1,23 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'sqlite3',
+    connection: {
+      filename: './data/assignments.db3'
+    },
+    useNullAsDefault: true,
+    migrations: {
+        directory: './data/migrations'
+    },
+    seeds: {
+        directory: './data/seeds'
+    },
+    pool: {
+        aftercreate: (connection, done) => {
+            connection.run('PRAGMA foreign_keys = ON', done);
+        },
+    },
+  },
+};
