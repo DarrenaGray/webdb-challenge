db99d0010ba461bb55bbe1be11863f7baad86b09
Darrena Gray <40064931+WhatTheSpork@users.noreply.github.com>
2019-05-03 13:31:15 -0400

    Imported the projects router and created the projects base url

--- api/server.js
+++ api/server.js
@@ -4,10 +4,13 @@ const morgan = require('morgan');
 
 const server = express();
 
+const projectsRouter = require('../projects/projects-router');
 
 server.use(express.json());
 server.use(helmet());
 server.use(morgan('dev'));
+
+server.use('/api/projects', projectsRouter)
 
 server.get('/', (req, res) => {
     res.send(`Connected!`);
