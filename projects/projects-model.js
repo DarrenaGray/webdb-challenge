baaf31e717b8f52a2783d74ff0395986afa114a1
Darrena Gray <40064931+WhatTheSpork@users.noreply.github.com>
2019-05-05 05:56:41 -0400

    Added CRUD for endpoints PUT and DELETE for updating and deleting projects

--- projects/projects-model.js
+++ projects/projects-model.js
@@ -5,6 +5,8 @@ module.exports = {
     get,
     getProjectActions,
     addProject,
+    updProject,
+    delProject,
 }
 
 function get() {
@@ -13,35 +15,35 @@ function get() {
 
 function get(id) {
     let query = db('projects as p');
-  
+
     if (id) {
       query.where('p.id', id).first();
-  
+
       const promises = [query, this.getProjectActions(id)];
-  
+
       return Promise.all(promises).then(function(results) {
         let [project, actions] = results;
-  
+
         if (project) {
           project.actions = actions;
-  
+
           return mappers.projectToBody(project);
         } else {
           return null;
         }
       });
-    }
-  
-    return query.then(projects => {
-      return projects.map(project => mappers.projectToBody(project));
-    });
   }
 
-  function getProjectActions(projectId) {
+    return query.then(projects => {
+      return projects.map(project => mappers.projectToBody(project));
+  });
+}
+
+function getProjectActions(projectId) {
     return db('actions')
       .where('project_id', projectId)
       .then(actions => actions.map(action => mappers.actionToBody(action)));
-  }
+}
 
 function addProject(project) {
     return db('projects')
@@ -49,4 +51,17 @@ function addProject(project) {
         .then(([id]) => {
             return this.get(id)
         });
+}
+
+function updProject(id, changes) {
+  return db('projects')
+    .where('id', id)
+    .update(changes)
+    .then(count => (count > 0 ? this.get(id) : null));
+}
+
+function delProject(id) {
+  return db('projects')
+    .where('id', id)
+    .del();
 }
--- projects/projects-router.js
+++ projects/projects-router.js
@@ -6,7 +6,11 @@ router.get('/', (req, res) => {
     Projects
         .get()
         .then(projects => {
-            res.status(200).json(projects);
+            if (projects.length !== 0) {
+                res.status(200).json(projects);
+            } else {
+                res.status(404).json({ message: "There are no projects." });
+            }
         })
         .catch(err => {
             res.status(500).json({ err, message: "There was a problem retrieving the projects." })
@@ -38,6 +42,39 @@ router.post('/', (req, res) => {
         })
         .catch(err => {
             res.status(500).json({ err, message: "There was a problem adding this project." });
+        });
+});
+
+router.put('/:id', (req, res) => {
+    const { id } = req.params;
+    const projectInfo = req.body;
+    Projects
+        .updProject(id, projectInfo)
+        .then(updProject => {
+            if (updProject) {
+                res.status(200).json({ updProject, message: "The project was succesfully updated!" });
+            } else {
+                res.status(404).json({ message: "The project with the specified ID could not be found." });
+            }
+        })
+        .catch(err => {
+            res.status(500).json({ err, message: "There was a problem updating the project." });
+        });
+})
+
+router.delete('/:id', (req, res) => {
+    const { id } = req.params
+    Projects
+        .delProject(id)
+        .then(delProject => {
+            if (delProject) {
+                res.status(200).json({ delProject, message: "The project was successfully deleted!" });
+            } else {
+                res.status(404).json({ message: "The project with the specified ID could not be found." });
+            }
+        })
+        .catch(err => {
+            res.status(500).json({ err, message: "There was a problem deleting the project." });
         });
 });
 
